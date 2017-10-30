f12a074af5a4810cf18c0ad3beefc72cb36a7665
Cássio André Wailer Trindade <cassiowt@gmail.com>
2017-10-30 11:11:18 -0200

    Update main.js

--- main.js
+++ main.js
@@ -11,5 +11,5 @@ function showSavedUser() {
 function limpar() {
     $('#txt-nome').val('').rer;
     $('#txt-email').val('');
-    alert('Limpado email e nome e id');
+    alert('Limpado nome e email');
 }
