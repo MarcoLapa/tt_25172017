6a8db5e2df6c2882e3dd172fad28c56443a158d5
Fábio Tramasoli <fabio@tramasoli.com>
2017-10-30 10:01:02 -0200

    Closes #7

---
+++ main.js
@@ -0,0 +1,9 @@
+/*
+    
+*/
+
+function showSavedUser(form) {
+   let nome = document.getElementById('txt-nome').value; 
+   let email = document.getElementById('txt-email').value; 
+   alert(nome+' <'+email+'>');
+}
